d30bdfa2d736db2ef779aaec907125fbf368cce5
Sean Callan <seancallan@gmail.com>
2015-04-10 15:50:17 -0700

    Include GA code

--- static/js/site.js
+++ static/js/site.js
@@ -11,12 +11,12 @@ $(function() {
 
 // Floating label headings for the contact form
 $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
+    $('body').on('input propertychange', '.floating-label-form-group', function(e) {
+        $(this).toggleClass('floating-label-form-group-with-value', !! $(e.target).val());
+    }).on('focus', '.floating-label-form-group', function() {
+        $(this).addClass('floating-label-form-group-with-focus');
+    }).on('blur', '.floating-label-form-group', function() {
+        $(this).removeClass('floating-label-form-group-with-focus');
     });
 });
 
@@ -29,3 +29,11 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-25045879-1', 'auto');
+ga('send', 'pageview');
