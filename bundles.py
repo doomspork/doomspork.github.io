815bb08784ebb9a64c892fb967803c8127b6c157
Sean Callan <seancallan@gmail.com>
2015-04-10 18:18:56 -0700

    Re-order bundles

--- bundles.py
+++ bundles.py
@@ -4,17 +4,17 @@ Define static webasset bundles.
 from flask.ext.assets import Bundle
 
 main_css = Bundle(
-    'css/vendor/bootstrap.css',
     'css/vendor/font-awesome.css',
+    'css/vendor/bootstrap.css',
     'css/site.css',
     filters='cssmin',
     output='main.css'
 )
 
 main_js = Bundle(
-    'js/vendor/bootstrap.js',
     'js/vendor/jquery-1.11.2.min.js',
     'js/vendor/jquery.easing.1.3.js',
+    'js/vendor/bootstrap.js',
     'js/animatedheader.js',
     'js/site.js',
     Bundle(
