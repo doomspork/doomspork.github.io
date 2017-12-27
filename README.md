28cbcbdcd4b0952dfb48b824033dbdf29d4626fd
Sean Callan <seancallan@gmail.com>
2017-12-27 14:59:04 -0700

    Long overdue updates

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # A homepage's home
 
-[![Build Status](https://travis-ci.org/doomspork/seancallan.com.svg)](https://travis-ci.org/doomspork/seancallan.com) [![Codacy Badge](https://www.codacy.com/project/badge/631f306d917a48da96786ef4303b2070)](https://www.codacy.com/app/seancallan/seancallan-com)
-
-This is the code behind [doomspork.com](http://doomspork.com) and [seancallan.com](http://seancallan.com).
+This is the code behind [seancallan.com](http://seancallan.com).
 
 Made with :heart: in Vim.
 
--- static/js/site.js
+++ static/js/site.js
@@ -1,25 +1,3 @@
-// jQuery easing setup
-$(function() {
-    $('body').on('click', '.page-scroll a', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Floating label headings for the contact form
-$(function() {
-    $('body').on('input propertychange', '.floating-label-form-group', function(e) {
-        $(this).toggleClass('floating-label-form-group-with-value', !! $(e.target).val());
-    }).on('focus', '.floating-label-form-group', function() {
-        $(this).addClass('floating-label-form-group-with-focus');
-    }).on('blur', '.floating-label-form-group', function() {
-        $(this).removeClass('floating-label-form-group-with-focus');
-    });
-});
-
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
@@ -30,10 +8,15 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+$('a.btn-learn-more').click(function() {
+  let text = $(this).text() === 'Learn More' ? 'Close' : 'Learn More';
+  $(this).text(text);
+})
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
- })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-25045879-1', 'auto');
 ga('send', 'pageview');
